c9bac5d5d3ddcb1347771d206dda0bf0efef022b
Sebastian Lopez <sebastianlopez_00@hotmail.com>
2024-11-07 23:03:47 -0300

    Update and rename basededatos.firestore to basededatos.js

--- basededatos.js
+++ basededatos.js
@@ -3,12 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
+  apiKey: "AIzaSyD60EKOLSgXraTs3Q9970PZxmCLD8IReHE",
+  authDomain: "registrodealumnos-411c5.firebaseapp.com",
+  databaseURL: "https://registrodealumnos-411c5-default-rtdb.firebaseio.com",
   projectId: "registrodealumnos-411c5",
-  storageBucket: "YOUR_STORAGE_BUCKET",
+  storageBucket: "registrodealumnos-411c5.firebasestorage.app",
   messagingSenderId: "40674985096",
-  appId: "YOUR_APP_ID"
+  appId: "1:40674985096:web:736af3f5ce063e29c18b3d",
+  measurementId: "G-SH5YRTBK2E"
 };
 
 // Initialize Firebase
